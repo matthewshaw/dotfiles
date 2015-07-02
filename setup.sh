126db37e84166554d6e57622a9bbfcea257ec0e8
Matt Shaw <matt@fiddleheadtech.com>
2015-07-02 10:33:53 -0300

    Fixing bug with spaces in paths

--- setup.sh
+++ setup.sh
@@ -2,20 +2,25 @@
 
 SCRIPT_LOCATION=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-. $SCRIPT_LOCATION/.bash_profile
+. "$SCRIPT_LOCATION/.bash_profile"
 
 NAME="$1"
 EMAIL="$2"
 
 if [[ "$NAME" != "" && "$EMAIL" != "" ]]; then
     if [[ $(checkdeps "/usr/bin/sed") -eq 0 ]]; then
-        sed -i "s/<NAME>/$NAME/g" $SCRIPT_LOCATION/.gitconfig
-        sed -i "s/<EMAIL>/$EMAIL/g" $SCRIPT_LOCATION/.gitconfig
+        sed -i "s/<NAME>/$NAME/g" "$SCRIPT_LOCATION"/.gitconfig
+        sed -i "s/<EMAIL>/$EMAIL/g" "$SCRIPT_LOCATION"/.gitconfig
     fi
 fi
 
-FILES_TO_LINK=`ls -d $SCRIPT_LOCATION/.* | egrep -v '(^|/)\.git$|(^|/)\.\.?$|^'$SCRIPT_LOCATION'$'`
+FILES_TO_LINK=`ls -d "$SCRIPT_LOCATION"/.* | egrep -v '(^|/)\.git$|(^|/)\.\.?$'`
 
+IFS=$(echo -en "\n\b")
 for file in $FILES_TO_LINK; do
-    ln -s $file ~/$(basename $file)
+  DEST=~/$(basename "$file")
+    if [ -e $DEST ]; then
+        rm $DEST;
+    fi
+    ln -s "$file" $DEST
 done
